f1ef44eeb5cc33787a90b99513ff725da5713043
danielaWaldeck95 <daniela@buildonline.co>
2022-09-18 17:03:24 +0200

    markup products list

--- app/index.php
+++ app/index.php
@@ -1,29 +1,32 @@
 <?php
-  echo '<h1>Yeah, it works!<h1>';
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
 
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
+  readfile('placard.html');
+
+//   echo '<h1>Esta es la entrega!<h1>';
+//   // phpinfo();
+//   $hostname = "db";
+//   $username = "admin";
+//   $password = "test";
+//   $db = "database";
+
+//   $conn = mysqli_connect($hostname,$username,$password,$db);
+//   if ($conn->connect_error) {
+//     die("Database connection failed: " . $conn->connect_error);
+//   }
 
 
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
+// $query = mysqli_query($conn, "SELECT * FROM usuarios")
+//    or die (mysqli_error($conn));
 
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
+// while ($row = mysqli_fetch_array($query)) {
+//   echo
+//    "<tr>
+//     <td>{$row['id']}</td>
+//     <td>{$row['nombre']}</td>
+//    </tr>";
    
 
-}
+// }
 
 ?>
