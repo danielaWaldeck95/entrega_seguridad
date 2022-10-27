83f99bf5134489a6b681f2a2f578f329ff9a2367
danielawaldeck95 <danielawaldeck95@gmail.com>
2022-10-27 15:48:23 +0200

    avoid using taliwind cdn

--- public/views/create-or-update-product.php
+++ public/views/create-or-update-product.php
@@ -61,7 +61,7 @@ if(isset($_POST) && !empty($_POST))
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://cdn.tailwindcss.com"></script>
+<link  href="/tailwind.css" rel="stylesheet" >
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body class="text-white">
