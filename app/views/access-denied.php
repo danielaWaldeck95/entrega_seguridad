9b38bff0d9e26c2d1185714d14868d168df70dff
danielaWaldeck95 <daniela@buildonline.co>
2022-10-30 16:07:54 +0100

    fix rotura del control de acceso

---
+++ app/views/access-denied.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/tailwind.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="text-white h-screen flex flex-col justify-center items-center bg-gray-900 min-h-screen">
+    <div class="w-full mx-auto bg-gray-800 rounded-lg shadow-xl md:mt-0 sm:max-w-md xl:p-0">
+        <h1 class="text-xl p-10 font-bold md:text-2xl flex flex-col justify-center items-center">
+            Acceso denegado
+        </h1>
+    </div>
+    <a href="/views/armario.php" class="text-rose-600 hover:underline mt-4">Volver al armario</a>
+</body>
+
+</html>
